27290291170f82f7a44120e64001a0a168953bc7
Zoli Prologic <zzoollii74@hotmail.com>
2020-09-12 14:09:35 +0200

    init

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*System.out.println("Hello World!");
+		System.out.println(Integer.parseInt(args[0])+ Integer.parseInt(args[1]) );*/
+		
+		System.out.println("Vezetéknév: " + args[0]);
+		System.out.println("Kersztnév: " + args[1]);
+		System.out.println("életkor: " + args[2]);
+		
+	}
+
+}
